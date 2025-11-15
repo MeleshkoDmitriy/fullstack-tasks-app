44ed372beb73ac0de213df068351915cd9a62f42
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-11-15 19:45:14 +0300

    feat: interceptor

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggingInterceptor } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   app.setGlobalPrefix('api/v1');
+
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   app.useGlobalPipes(
     new ValidationPipe({
