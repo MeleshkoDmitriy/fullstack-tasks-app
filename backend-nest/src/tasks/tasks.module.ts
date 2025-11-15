fd879f4c2ef4cd2c08dd0b38eb8a6eed43468514
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-11-15 18:21:12 +0300

    feat: basic middleware

--- backend-nest/src/tasks/tasks.module.ts
+++ backend-nest/src/tasks/tasks.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { LoggerMiddleware } from 'src/common';
 
 @Module({
+  imports: [],
   controllers: [TasksController],
   providers: [TasksService],
 })
-export class TasksModule {}
+export class TasksModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    // consumer.apply(LoggerMiddleware).forRoutes('tasks');
+  }
+}
