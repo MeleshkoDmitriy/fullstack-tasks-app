6228a53086fd1847290964c2fc08a4f491e74164
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2025-12-06 19:37:34 +0300

    feat: basic get request

--- mobile-expo/src/screens/tasks/TasksListScreen.tsx
+++ mobile-expo/src/screens/tasks/TasksListScreen.tsx
@@ -1,9 +1,37 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { TTask } from '../../types';
+import { apiConfig } from '../../services/api/apiConfig';
 
 export const TasksListScreen = () => {
+  const [tasks, setTasks] = useState<TTask[]>([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await apiConfig.get<TTask[]>('/tasks');
+        setTasks(response.data);
+      } catch (error) {
+        console.log('Error fetching', error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
   return (
     <View>
       <Text>TasksListScreen</Text>
+      <FlatList
+        data={tasks}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.status}</Text>
+          </View>
+        )}
+        keyExtractor={(tasks) => tasks.id}
+      />
     </View>
   );
 };
---
+++ mobile-expo/src/types/task.types.ts
@@ -0,0 +1,56 @@
+export const enum EnumTaskTag {
+  WORK = 'work',
+  HOME = 'home',
+  PERSONAL = 'personal',
+  HEALTH = 'health',
+  SHOPPING = 'shopping',
+  FINANCE = 'finance',
+  EDUCATION = 'education',
+  FAMILY = 'family',
+  SOCIAL = 'social',
+  TRAVEL = 'travel',
+  FOOD = 'food',
+  CAR = 'car',
+  PET = 'pet',
+  HOBBY = 'hobby',
+  SPORT = 'sport',
+}
+
+export const enum EnumTaskPriority {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high',
+}
+
+export const enum EnumTaskStatus {
+  TODO = 'to-do',
+  IN_PROGRESS = 'in-progress',
+  DONE = 'done',
+}
+
+export type TTask = {
+  id: string;
+  title: string;
+  description: string;
+  tags: EnumTaskTag[];
+  priority: EnumTaskPriority;
+  status: EnumTaskStatus;
+  isBlocked: boolean;
+  createdAt: Date;
+  updateAt: Date;
+};
+
+
+// export type CreateTaskDto = {
+// }
+
+// export type UpdateTaskDto = {
+// }
+
+export type TGetTasksFilters = {
+  search?: string;
+  status?: EnumTaskStatus;
+  priority?: EnumTaskPriority;
+  tags?: EnumTaskTag[];
+  isBlocked?: boolean;
+}
